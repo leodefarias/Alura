038075aa14be13f82f0e3964cb465783f1597ed4
leodefarias <lf25079@gmail.com>
2024-05-06 20:06:01 -0300

    Orientação de objetos 2

--- Python/OO-Sabor-express/app.py
+++ Python/OO-Sabor-express/app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.avaliar('Gui', 10)
-restaurante_praca.avaliar('Leo', 8)
-restaurante_praca.avaliar('Lilica', 1)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurante()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
